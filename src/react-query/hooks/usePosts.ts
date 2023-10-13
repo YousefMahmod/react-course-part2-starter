67cc51e8a820270005c9919fe554674dd0c42e4a
Youssef Mahmoud <youssef.shubra@yahoo.com>
2023-10-13 11:53:44 +0300

    Refactor: postList using react query , customHook

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+}
+const usePosts = () => {
+    const fetchPosts = () => {
+        return axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.data);
+    };
+    return useQuery<Post[], Error>({ queryKey: ["posts"], queryFn: fetchPosts });
+}
+
+export default usePosts;
