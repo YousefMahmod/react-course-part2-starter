0c11d3be3fe06c11a76f64f91437aee4a8c33072
Youssef Mahmoud <youssef.shubra@yahoo.com>
2023-10-13 12:26:44 +0300

    Add paramaterized query to fetch user posts

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -2,18 +2,22 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
 }
-const usePosts = () => {
-    const fetchPosts = () => {
-        return axios
-        .get("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.data);
-    };
-    return useQuery<Post[], Error>({ queryKey: ["posts"], queryFn: fetchPosts });
-}
+const usePosts = (userId?: number) => {
+  const fetchPosts = () => {
+    return axios
+      .get("https://jsonplaceholder.typicode.com/posts", { params: { userId } })
+      .then((res) => res.data);
+  };
+  return useQuery<Post[], Error>({
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 10 * 1000,
+  });
+};
 
 export default usePosts;
