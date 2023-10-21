29ef7614d861a86c5a8a88281f0a8d133f6fc51c
Youssef Mahmoud <youssef.shubra@yahoo.com>
2023-10-21 15:17:00 +0300

    add tasksReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 import LoginStatus from "./state-management/LoginStatus";
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <LoginStatus />;
+  return <TaskList />;
 }
 
 export default App;
