a33b398ae3a26e864cfee2d6062845acc424355b
Youssef Mahmoud <youssef.shubra@yahoo.com>
2023-10-21 15:31:18 +0300

    add tasksContext to share tasks between taskList and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useReducer } from "react";
 import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
+import HomePage from "./state-management/HomePage";
 import LoginStatus from "./state-management/LoginStatus";
+import NavBar from "./state-management/NavBar";
 import TaskList from "./state-management/TaskList";
+import TasksContext from "./state-management/contexts/tasksContext";
+import tasksReducer from "./state-management/reducers/tasksReducer";
 
 function App() {
-  return <TaskList />;
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      <NavBar />
+      <HomePage />
+    </TasksContext.Provider>
+  );
 }
 
 export default App;
